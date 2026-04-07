43ced5205141d246204f3f4a8a8ac121015b4032
Syedain Iqbal Shigri <sishigree@gmail.com>
2026-04-07 18:11:42 +0500

    react initial

---
+++ Learning-hub/src/Page/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>Welcome to About</div>
+  )
+}
+
+export default About
---
+++ Learning-hub/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link, Links } from 'react-router-dom'
+
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </div>
+  )
+}
+
+export default Header
